b59c03f8464f59bbe21319fc83692822640e34eb
zimo <461009747@qq.com>
2020-11-25 23:05:12 +0800

    release v1.0.1

--- js/ribbon.js
+++ js/ribbon.js
@@ -1,10 +1,11 @@
 const ide = 'WPS'
 const ide_version = wps.Application.Build
 const plugin = 'WPS-wakatime'
-const plugin_version = '1.0.0'
+const plugin_version = '1.0.1'
 
-var lastAction = 0,
-    lastFile = undefined;
+var lastAction = 0
+var lastFile = undefined
+var debug = false
 
 //这个函数在整个wps加载项中是第一个执行的
 function OnAddinLoad(ribbonUI) {
@@ -17,20 +18,56 @@ function OnAddinLoad(ribbonUI) {
     }
 
     var api_key = read_api_key()
-    console.log(api_key)
+    if (debug) {
+        log(api_key)
+    }
 
+    // save
     wps.ApiEvent.AddApiEventListener("DocumentBeforeSave", (doc) => {
+        if (debug) {
+            log('DocumentBeforeSave')
+        }
         handleAction(true);
     });
     wps.ApiEvent.AddApiEventListener("WorkbookBeforeSave", (doc) => {
+        if (debug) {
+            log('WorkbookBeforeSave')
+        }
         handleAction(true);
     });
     wps.ApiEvent.AddApiEventListener("PresentationBeforeSave", (doc) => {
+        if (debug) {
+            log('PresentationBeforeSave')
+        }
         handleAction(true);
     });
 
+    // open
+    wps.ApiEvent.AddApiEventListener("DocumentOpen", (doc) => {
+        if (debug) {
+            log('DocumentOpen')
+        }
+        handleAction(true);
+    });
+    wps.ApiEvent.AddApiEventListener("WorkbookOpen", (doc) => {
+        if (debug) {
+            log('WorkbookOpen')
+        }
+        handleAction(true);
+    });
+    wps.ApiEvent.AddApiEventListener("NewPresentation", (doc) => {
+        if (debug) {
+            log('PresentationOpen')
+        }
+        handleAction(true);
+    });
+
+    // window changed
     wps.ApiEvent.AddApiEventListener("WindowActivate", (doc) => {
-        handleAction();
+        if (debug) {
+            log('WindowActivate')
+        }
+        handleAction(true);
     });
 
 
@@ -46,7 +83,9 @@ function read_api_key() {
         var api_key = wps.FileSystem.readAsBinaryString(config_path)
         return partten.exec(api_key)[0]
     } catch (e) {
-        console.log(e)
+        if (debug) {
+            log(e)
+        }
         // 没有配置api_key
         wps.ShowDialog(GetUrlPath() + '/input.html', '输入api_key')
     }
@@ -80,7 +119,9 @@ function sendHeartbeat(file, time, project, language, isWrite, lines) {
             'Authorization': 'Basic ' + window.btoa(api_key)
         },
         success: function (res) {
-            console.log(res)
+            if (debug) {
+                log(res)
+            }
         }
     })
     lastAction = time;
@@ -89,13 +130,29 @@ function sendHeartbeat(file, time, project, language, isWrite, lines) {
 
 function handleAction(isWrite) {
     var api_key = read_api_key()
-    console.log(api_key)
     var currentDocument = wps.Application.ActiveDocument
+    if (currentDocument == undefined) {
+        currentDocument = wps.Application.ActiveWorkbook
+    }
+    if (currentDocument == undefined) {
+        currentDocument = wps.Application.ActivePresentation
+    }
     var time = Date.now()
     if (isWrite || enoughTimePassed() || lastFile !== currentDocument.FullName) {
         var language = undefined
         var project = undefined
-        var lines = currentDocument.lineCount ? currentDocument.lineCount : undefined
+        var lines = undefined
         sendHeartbeat(currentDocument.FullName, time, project, language, isWrite, lines, api_key)
     }
+}
+
+function log(content) {
+    console.log(content)
+    var tmpdir = wps.Env.GetTempPath()
+    var logdir = tmpdir + '/wps-wakatime'
+    if (!wps.FileSystem.Exists(logdir)) {
+        wps.FileSystem.Mkdir(logdir)
+    }
+    var logfile = logdir + '/' + Date.now() + '.log'
+    wps.FileSystem.writeAsBinaryString(logfile, JSON.stringify(content))
 }
